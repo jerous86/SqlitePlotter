f054b2ba9b2366bcc0fddec744319a981a4ddbcb
jerous <jerous@gmail.com>
2022-12-21 18:37:05 +0100

    enable qDebug() on some systems

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 set -uex
 
+export QT_LOGGING_RULES="*.debug=true;qt.qpa.input*.debug=false;qt.widgets.gestures.debug=false"
 ROOT=$(cd "$(dirname "$0")"; echo $PWD)
 $ROOT/build.sh SqlitePlotter importers
 $ROOT/bin/SqlitePlotter "${@}"
